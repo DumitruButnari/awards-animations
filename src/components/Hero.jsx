0c4492b741ac98a1043f198034a63d371737e57e
DumitruButnari <dimkalanu@gmail.com>
2025-01-04 18:59:18 +0200

    finish hero-section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,21 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import Loading from "./Loading.jsx";
+import {ScrollTrigger} from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedVideo, setLoadedVideo] = useState(0);
+    const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 3;
+    const totalVideos = 4;
     const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
     const nextVideoRef = useRef();
     const getVideoSrc = (index) => {
@@ -23,9 +28,18 @@ const Hero = () => {
     }
 
     const handleVideoLoad= () => {
-        setLoadedVideo((prev)=>prev + 1);
+        setLoadedVideos((prev)=>prev + 1);
     }
 
+    useEffect(() => {
+        if (loadedVideos === totalVideos-1) {
+            setIsLoading(false);
+        }
+    }, [loadedVideos]);
+
+    // Gsap Animations
+
+    // Video animation
     useGSAP(()=>{
         if (hasClicked) {
             gsap.set('#next-video', {visibility: 'visible'});
@@ -47,7 +61,29 @@ const Hero = () => {
         }
     }, {dependencies: [currentIndex], revertOnUpdate:true})
 
+    // Scroll on hero section animation
+
+    useGSAP(()=>{
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(50% 0%, 93% 1%, 100% 43%, 87% 89%, 68% 100%, 36% 84%, 2% 91%, 0% 43%, 3% 15%)',
+            borderRadius: '10% 0% 40% 50%',
+        });
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 0% 100%, 0% 100%, 0% 100%)',
+            borderRadius: '0 0 0 0',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        });
+    });
+
   return <div className={'relative h-dvh w-screen overflow-x-hidden'}>
+      {isLoading && <Loading/>}
+      <h2 className={'special-font hero-heading text-black absolute lg:top-[6rem] lg:left-10'}>redefi<b>n</b>e</h2>
       <div id={'video-frame'}
            className={'relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'}>
           <div>
@@ -99,7 +135,7 @@ const Hero = () => {
               </div>
           </div>
       </div>
-      <h1 className={'special-font hero-heading absolute right-5 bottom-5 text-blue-75'}><b>Gaming</b></h1>
+      <h1 className={'special-font hero-heading absolute right-5 bottom-5 text-black'}><b>Gaming</b></h1>
   </div>;
 };
 export default Hero;
